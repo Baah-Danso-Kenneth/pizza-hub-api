710c1d9bf0a2c818a3d887860eb013f6053f8142
Kenneth <kwadwobaahdanso@gmail.com>
2023-12-06 14:01:59 +0000

    modiefied regex

--- stores/models.py
+++ stores/models.py
@@ -7,12 +7,13 @@ class Pizzeria(models.Model):
     city = models.CharField(max_length=300, blank=True)
     state= models.CharField(max_length=2, null=True, blank=True)
     zip_code = models.IntegerField(blank=True, default=0)
-    phone_number=models.CharField(
-        validators=[RegexValidator(regex=r'^\1?\d{9,10}$')],
-        max_length=10,
-        blank=True
-    )
+    phone_number = models.CharField(validators=[RegexValidator(regex=r'^\1?\d{9,10}$')],
+         max_length=10,
+     blank=True)
     description = models.TextField(blank=True)
     logo_image = models.ImageField(upload_to='pizzarianImages', blank=True, default='pizz')
     email = models.EmailField(max_length=245, blank=True)
     active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{}, {}".format(self.pizzeria_name, self.city)
