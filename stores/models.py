6db9e33a048f8e18079edcb06c102822a4576d51
Kenneth <kwadwobaahdanso@gmail.com>
2023-12-06 13:12:47 +0000

    added fields to Pizzeria Class

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Pizzeria(models.Model):
+    pizzeria_name=models.CharField(max_length=200, blank=False)
+    street = models.CharField(max_length=400, blank=True)
+    city = models.CharField(max_length=300, blank=True)
+    state= models.CharField(max_length=2, null=True, blank=True)
+    zip_code = models.IntegerField(blank=True, default=0)
+    phone_number=models.CharField(
+        validators=[RegexValidator(regex=r'^\1?\d{9,10}$')],
+        max_length=10,
+        blank=True
+    )
+    description = models.TextField(blank=True)
+    logo_image = models.ImageField(upload_to='pizzarianImages', blank=True, default='pizz')
+    email = models.EmailField(max_length=245, blank=True)
+    active = models.BooleanField(default=False)
