3416a907a009d1ea32a44f8b57cda2b200bb5f08
Kenneth <kwadwobaahdanso@gmail.com>
2023-12-07 12:22:00 +0000

    views.py file created to transfer the data to the routers.py

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import PizzeriaSerializer
+from rest_framework import generics
+from .models import Pizzeria
 
-# Create your views here.
+
+class PizzeriaListAPIView(generics.ListAPIView):
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaSerializer
